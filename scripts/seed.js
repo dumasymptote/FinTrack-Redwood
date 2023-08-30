278e188982e8a8a307c8342524b83501b67fea7d
Mark Guthrie <dumasymptote@gmail.com>
2023-08-30 12:59:23 -0500

    added tailwind and tried to start nav layout

--- scripts/seed.js
+++ scripts/seed.js
@@ -1,4 +1,5 @@
 import { db } from 'api/src/lib/db'
+import { hashPassword } from '@redwoodjs/auth-dbauth-api'
 
 export default async () => {
   try {
@@ -12,7 +13,7 @@ export default async () => {
       // To try this example data with the UserExample model in schema.prisma,
       // uncomment the lines below and run 'yarn rw prisma migrate dev'
       //
-      // { name: 'alice', email: 'alice@example.com' },
+       //{ email: 'mguthriejr@gmail.com',name: 'alice', email: 'alice@example.com' },
       // { name: 'mark', email: 'mark@example.com' },
       // { name: 'jackie', email: 'jackie@example.com' },
       // { name: 'bob', email: 'bob@example.com' },
@@ -28,7 +29,7 @@ export default async () => {
       // Change to match your data model and seeding needs
       //
       data.map(async (data) => {
-        const record = await db.userExample.create({ data })
+        const record = await db.user.create({ data })
         console.log(record)
       })
     )
@@ -36,25 +37,21 @@ export default async () => {
     // If using dbAuth and seeding users, you'll need to add a `hashedPassword`
     // and associated `salt` to their record. Here's how to create them using
     // the same algorithm that dbAuth uses internally:
-    //
-    //   import { hashPassword } from '@redwoodjs/auth-dbauth-api'
-    //
-    //   const users = [
-    //     { name: 'john', email: 'john@example.com', password: 'secret1' },
-    //     { name: 'jane', email: 'jane@example.com', password: 'secret2' }
-    //   ]
-    //
-    //   for (const user of users) {
-    //     const [hashedPassword, salt] = hashPassword(user.password)
-    //     await db.user.create({
-    //       data: {
-    //         name: user.name,
-    //         email: user.email,
-    //         hashedPassword,
-    //         salt
-    //       }
-    //     })
-    //   }
+      const users = [
+         { name: 'Mark G.', email: 'testEmail@email.com', password: 'testPassword' }
+      ]
+    // Create seeded user with test data
+       for (const user of users) {
+         const [hashedPassword, salt] = hashPassword(user.password)
+         await db.user.create({
+           data: {
+             name: user.name,
+             email: user.email,
+             hashedPassword,
+             salt
+           }
+         })
+       }
   } catch (error) {
     console.warn('Please define your seed data.')
     console.error(error)
--- web/src/layouts/UnauthLayout/UnauthLayout.jsx
+++ web/src/layouts/UnauthLayout/UnauthLayout.jsx
@@ -15,15 +15,25 @@ const UnauthLayout = ({ children }) => {
   return (
     <>
       <header>
-        <nav className="">
-          <ul>
-            <li>
-              <Link to={routes.home()}>Home</Link>
-            </li>
-            <li>
-              <Link to={routes.login()}>Login</Link>
-            </li>
-          </ul>
+        <nav className="bg-white border-gray-200">
+          <div className='max-w-screen-xl flex items-center justify-between mx-auto p-4'>
+            <a href="https://fintrack.dumasymptote.com" className='flex items-center'>
+              <span>Fin Track</span>
+            </a>
+            <div className='flex md:order-2'>
+              <button type='button' className='text-white bg-blue-700 hover:to-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'>Dumasymptote(dot)com</button>
+            </div>
+            <div className='items-center justify-between hidden w-full md:flex md:w-auto mdLorder-1'>
+              <ul className='flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700'>
+                <li>
+                  <Link className='block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500' to={routes.home()}>Home</Link>
+                </li>
+                <li>
+                  <Link className='block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500' to={routes.login()}>Login</Link>
+                </li>
+              </ul>
+            </div>
+          </div>
         </nav>
       </header>
       <main>{children}</main>
