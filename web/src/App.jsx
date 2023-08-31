d46421e4a9ca6d1e2216d4b61ebe62d780932ebf
Mark Guthrie <dumasymptote@gmail.com>
2023-08-30 20:20:26 -0500

    finally got these damn classes working

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,9 +1,6 @@
 import { FatalErrorBoundary, RedwoodProvider } from '@redwoodjs/web'
 import { RedwoodApolloProvider } from '@redwoodjs/web/apollo'
 
-import { MantineProvider } from '@mantine/core'
-import * as theme from 'config/mantine.config'
-
 import FatalErrorPage from 'src/pages/FatalErrorPage'
 import Routes from 'src/Routes'
 
@@ -15,13 +12,11 @@ import './index.css'
 const App = () => (
   <FatalErrorBoundary page={FatalErrorPage}>
     <RedwoodProvider titleTemplate="%PageTitle | %AppTitle">
-      <MantineProvider theme={theme}>
         <AuthProvider>
           <RedwoodApolloProvider useAuth={useAuth}>
             <Routes />
           </RedwoodApolloProvider>
         </AuthProvider>
-      </MantineProvider>
     </RedwoodProvider>
   </FatalErrorBoundary>
 )
--- web/src/layouts/UnauthLayout/UnauthLayout.jsx
+++ web/src/layouts/UnauthLayout/UnauthLayout.jsx
@@ -1,40 +1,50 @@
-import {
-  Menu,
-  Center,
-  Header,
-  Container,
-  Group,
-  Button,
-  Burger,
-  rem,
-  createStyles,
-} from '@mantine/core'
-
 import { Link, routes } from '@redwoodjs/router'
+
 const UnauthLayout = ({ children }) => {
   return (
     <>
       <header>
-        <nav className="bg-white border-gray-200">
-          <div className='max-w-screen-xl flex items-center justify-between mx-auto p-4'>
-            <a href="https://fintrack.dumasymptote.com" className='flex items-center'>
-              <span>Fin Track</span>
-            </a>
-            <div className='flex md:order-2'>
-              <button type='button' className='text-white bg-blue-700 hover:to-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'>Dumasymptote(dot)com</button>
+        <nav className='bg-gray-800'>
+          <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+            <div className="relative flex h-16 items-center justify-between">
+              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                <button type="button" clasNames="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
+                  <span className="absolute -inset-0.5"></span>
+                  <span className="sr-only">Open main menu</span>
+                  <svg className="block h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                  </svg>
+                  <svg className="hidden h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                  </svg>
+                </button>
+              </div>
+              <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+                <div className="flex flex-shrink-0 items-center">
+                  <Link className='text-gray-200' to={routes.home()}>fintracker(at)dumasymptote</Link>
+                </div>
+                <div className="hidden sm:ml-6 sm:block">
+                  <div className="flex space-x-4">
+                    <Link to={routes.home()} className="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium hover:text-blue-800">Home</Link>
+                    <Link to={routes.home()} className="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium">About</Link>
+                    <Link to={routes.home()} className="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium">Contact</Link>
+                    <Link to={routes.home()} className="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium">Blog</Link>
+                  </div>
+                </div>
+              </div>
             </div>
-            <div className='items-center justify-between hidden w-full md:flex md:w-auto mdLorder-1'>
-              <ul className='flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700'>
-                <li>
-                  <Link className='block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500' to={routes.home()}>Home</Link>
-                </li>
-                <li>
-                  <Link className='block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500' to={routes.login()}>Login</Link>
-                </li>
-              </ul>
+          </div>
+
+          <div className="sm:hidden" id="mobile-menu">
+            <div className="space-y-1 px-2 pb-3 pt-2">
+              <a href="#" className="bg-gray-900 text-white block rounded-md px-3 py-2 text-base font-medium" aria-current="page">Dashboard</a>
+              <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Team</a>
+              <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Projects</a>
+              <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Calendar</a>
             </div>
           </div>
         </nav>
+
       </header>
       <main>{children}</main>
     </>
