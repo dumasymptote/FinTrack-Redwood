f90c6432491a1facb964bef11ce6ca69281a422b
Mark Guthrie <dumasymptote@gmail.com>
2023-09-01 08:31:49 -0500

    upgrade to redwood version and navbar work

--- web/src/components/Navbar/Navbar.jsx
+++ web/src/components/Navbar/Navbar.jsx
@@ -1,74 +1,58 @@
-import { Link, routes } from '@redwoodjs/router'
+import { useState } from 'react'
 
+import { Link, routes } from '@redwoodjs/router'
 const Navbar = () => {
   const navlinks = [
     { url: routes.home(), text: 'Home' },
     { url: routes.login(), text: 'Login' },
   ]
   const navItems = navlinks.map((navLink) => (
-    <Link
-      key={navLink.text}
-      to={navLink.url}
-      className="px-3 py-2 text-sm font-medium hover:rounded-md hover:bg-slate-200 hover:text-black"
-    >
-      {navLink.text}
-    </Link>
+    <li key={navLink.text} className="nav-item">
+      <Link
+        to={navLink.url}
+        className="flex items-center px-3 py-2 text-xs font-bold uppercase leading-snug text-slate-400 hover:opacity-75"
+      >
+        {navLink.text}
+      </Link>
+    </li>
   ))
-
+  const [navbarOpen, setNavbarOpen] = useState(false)
   return (
     <>
-      <nav className="bg-slate-800">
-        <div className="mx-auto max-w-7xl px-2 text-lime-300 sm:px-6 lg:px-8">
-          <div className="relative flex h-16 items-center justify-between">
-            <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-              <button
-                type="button"
-                className="relative inline-flex items-center justify-center rounded-md p-2 text-lime-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
-                aria-controls="mobile-menu"
-                aria-expanded="false"
+      <nav className="relative mb-3 flex flex-wrap items-center justify-between bg-slate-800 px-2 py-3">
+        <div className="container mx-auto flex flex-wrap items-center justify-between px-4">
+          <div className="relative flex w-full justify-between lg:static lg:block lg:w-auto lg:justify-start">
+            <Link
+              className="mr-4 inline-block whitespace-nowrap py-2 text-sm font-bold uppercase leading-relaxed text-slate-400"
+              to={routes.home()}
+            >
+              fintracker(at)dumasymptote
+            </Link>
+            <button
+              className="block cursor-pointer rounded border border-solid border-transparent bg-transparent px-3 py-1 text-xl leading-none text-slate-400 outline-none focus:outline-none lg:hidden"
+              type="button"
+              onClick={() => setNavbarOpen(!navbarOpen)}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                height="1em"
+                viewBox="0 0 448 512"
+                className="fill-slate-400"
               >
-                <span className="absolute -inset-0.5"></span>
-                <span className="sr-only">Open main menu</span>
-                <svg
-                  className="block h-6 w-6"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth="1.5"
-                  stroke="currentColor"
-                  aria-hidden="true"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-                  />
-                </svg>
-                <svg
-                  className="hidden h-6 w-6"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth="1.5"
-                  stroke="currentColor"
-                  aria-hidden="true"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M6 18L18 6M6 6l12 12"
-                  />
-                </svg>
-              </button>
-            </div>
-            <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-              <div className="flex flex-shrink-0 items-center">
-                <Link className="" to={routes.home()}>
-                  fintracker(at)dumasymptote
-                </Link>
-              </div>
-              <div className="hidden sm:ml-6 sm:block">
-                <div className="flex space-x-4">{navItems}</div>
-              </div>
-            </div>
+                <path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z" />
+              </svg>
+            </button>
+          </div>
+          <div
+            className={
+              'flex-grow items-center lg:flex' +
+              (navbarOpen ? ' flex' : ' hidden')
+            }
+            id="navbar"
+          >
+            <ul className="flex list-none flex-col lg:ml-auto lg:flex-row">
+              {navItems}
+            </ul>
           </div>
         </div>
       </nav>
